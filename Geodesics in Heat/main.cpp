d7627e2dd0f0dbb1ea7de4dd0e8155a72a05f297
arukadya <a19.sj7x@g.chuo-u.ac.jp>
2023-07-20 15:04:29 +0900

    Initial Commit

---
+++ Geodesics in Heat/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Geodesics in Heat
+//
+//  Created by 須之内俊樹 on 2023/07/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
